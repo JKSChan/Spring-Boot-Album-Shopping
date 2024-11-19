c483578b389adabe3b6dc0a2a47e7d26a82dd384
Jackson Chan <jackson20337@yahoo.com.tw>
2024-11-19 19:54:46 +0800

    初始化 Spring Boot Project

---
+++ src/main/java/com/jacksonchan/SpringBootAlbumShoppingApplication.java
@@ -0,0 +1,11 @@
+package com.jacksonchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlbumShoppingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAlbumShoppingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jacksonchan/SpringBootAlbumShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacksonchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAlbumShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
